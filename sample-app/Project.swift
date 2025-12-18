b2e19045a714e5b8334179ec49afea9f2cee274f
abramov_ia <ivan_abramov@itms.tech>
2025-12-18 16:27:16 +0300

    Add sampel app

---
+++ sample-app/Project.swift
@@ -0,0 +1,39 @@
+import ProjectDescription
+
+let project = Project(
+    name: "SampleApp",
+    targets: [
+        .target(
+            name: "MadSDKSampleApp",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "com.mads.SampleApp",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                ]
+            ),
+            sources: ["SampleApp/Sources/**"],
+            resources: ["SampleApp/Resources/**"],
+            dependencies: [
+                .package(product: "MadSDK", type: .runtime)
+            ]
+        ),
+        .target(
+            name: "SampleAppTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.SampleAppTests",
+            infoPlist: .default,
+            sources: ["SampleApp/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "MadSDKSampleApp")]
+        ),
+    ],
+    packages: [
+        .remote(
+            url: "https://github.com/magnit-tech/mads-ios-sdk",
+            requirement: .upToNextMajor(from: "0.0.1")
+        )
+    ]
+)
